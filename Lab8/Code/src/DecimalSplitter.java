c52b5a8ea7e6b6e6265df1678555c2420f3502e0
Arpit Palo <arpitpalo@gmail.com>
2020-07-16 13:16:37 +0530

    Completed DecimalSplitter functions

--- Lab8/Code/src/DecimalSplitter.java
+++ Lab8/Code/src/DecimalSplitter.java
@@ -1,13 +1,17 @@
 public class DecimalSplitter {
 	static boolean isOdd(int num) {
 //Complete the logic
+		return (num%2 == 1);
 	}
 	static int getWhole(double num) {
 //Complete the logic
+		return (int)num;
 	}
 	
 	static double getFraction(double num) {
 //Complete the logic
+		return num - (int)num;
+		
 	}
 }
 
