741277e052590ed2118c979a7797ec01932d71bd
Arpit Palo <arpitpalo@gmail.com>
2020-07-16 21:48:47 +0530

    Completed swap function

--- Lab5/Code/src/SwapDemo.java
+++ Lab5/Code/src/SwapDemo.java
@@ -4,12 +4,17 @@ public class SwapDemo {
 		int arg2 = 200;
 	
 	// make call to swapValues() and displayValues()
-
+		displayValues("Values before swapping are:", arg1, arg2);
+		swapValues(arg1, arg2);
+		displayValues("Values after swapping are:", arg1, arg2);
 	}
 
 	private static void swapValues(int arg1,int arg2) {
 	
 	// Write logic to swap values
+		int temp = arg1;
+		arg1 = arg2;
+		arg2 = temp;
 
 	}
 	
