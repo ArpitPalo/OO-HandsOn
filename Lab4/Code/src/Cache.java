4957d81ffa8c135b943cc0c377bdcc656bdd0f27
Arpit Palo <arpitpalo@gmail.com>
2020-07-16 21:47:36 +0530

    Completed getMaxCapacity() function

--- Lab4/Code/src/Cache.java
+++ Lab4/Code/src/Cache.java
@@ -1,9 +1,15 @@
 public class Cache {
 	private static int MAX_CAPACITY = 0;
-	
+	static int count = 0;
 	// Implement static block
 
 	static int getMaxCapacity() {
+		++count;
+		if(count == 1) {
+			System.out.print("Enter Max capacity : ");
+			int capacity = Console.readInt();
+			MAX_CAPACITY = capacity;
+		}
 		System.out.println("Returning MAX_CAPACITY");
 		return MAX_CAPACITY;
 	}
